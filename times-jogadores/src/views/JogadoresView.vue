06e02b38a1fcbae43066875efc0887fcf472fe4a
AmandaPires <102668847+AmandaPires17@users.noreply.github.com>
2022-06-09 11:39:01 -0300

    Add files via upload

---
+++ times-jogadores/src/views/JogadoresView.vue
@@ -0,0 +1,62 @@
+<script>
+import {v4 as uuid} from "uuid";
+export default {
+  data() {
+    return {
+      novo_jogador: "",
+      jogadores: [
+        {id: "d6aa0cb0-4232-49a3-8698-5131e28d94b6", name: "Jogador 1", idade: "X", timeid: "1", acoes:"?"  },
+        {id: "54413720-559a-44c2-8019-06d360482c7d", name: "Jogador 2", idade: "X", timeid: "1", acoes:"?"  },
+        {id: "2d66ac28-a63c-471c-b301-abacff8dd2fb", name: "Jogador 3", idade: "X", timeid: "2", acoes:"?"  },
+        {id: "29da6b76-21f1-4df6-898c-e4705fd819df", name: "Jogador 4", idade: "X", timeid: "4", acoes:"?"  },
+      ],
+    };
+  },
+  methods: {
+    salvar() {
+      const novo_id=uuid();
+      this.jogadores.push({
+        id: novo_id,
+        name: this.novo_jogadores,
+      });
+    },
+  },
+}
+</script>
+<template>
+ <div class="container">
+  <div class="title">
+    <h2>Gerenciamento de jogadores</h2>
+  </div>
+  <div class="form-input">
+    <input type="text" placeholder="Nome" v-model="novo_jogador" /> 
+    <input type="text" placeholder="Time" v-model="">
+    <button>salvar</button>
+  </div>
+  <div class="list-itens">
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Idade</th>
+          <th>Time ID</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="jogador in jogadores" :key="jogador.id">
+          <td>1</td>
+          <td>Jogador 1</td>
+          <td>X</td>
+          <td>1</td>
+          <td>???</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+</template>
+<style scoped>
+
+</style>
---
+++ times-jogadores/src/views/TimesView.vue
@@ -0,0 +1,58 @@
+<script>
+import {v4 as uuid} from "uuid";
+export default {
+  data() {
+    return {
+      novo_time: "",
+      times: [
+        {id: "935d917f-9612-4473-80f5-ebf77eaba923", name: "Time 1"},
+        {id: "3aa79f04-9cf6-4248-b772-b831c1ea6c8e", name: "Time 2"},
+        {id: "72f44dc5-f276-46c2-9463-ed058a8ef667", name: "Time 3"},
+        {id: "b667a3b3-3fb7-40af-ad89-96ff020f955c", name: "Time 4"},
+      ],
+    };
+  },
+  methods: {
+    salvar() {
+      const novo_id=uuid();
+      this.times.push({
+        id: novo_id,
+        name: this.novo_time,
+      });
+    },
+  },
+};
+</script>
+
+<template>
+<div class="container">
+  <div class="title">
+    <h2>Gerenciamento de times</h2>
+  </div>
+  <div class="form-input">
+    <input type="text" v-model="novo_time" />
+    <button @click="salvar">salvar</button>
+  </div>
+  <div class="list-itens">
+    
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="time in times" :key="time.id">
+          <td>{{ time.id }}</td>
+          <td>{{ time.name }}</td>
+          <td>???</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+</template>
+
+<style></style>
